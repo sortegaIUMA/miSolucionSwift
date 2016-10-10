e8a84d7b28b670f76d2e0c46f45efbb49f6f3f1b
Samuel Ortega Sarmiento <samuelortegasarmiento@sortega.home>
2016-10-10 22:33:20 +0100

    Mi solución

--- miSolucionAlReto.playground/Contents.swift
+++ miSolucionAlReto.playground/Contents.swift
@@ -2,4 +2,21 @@
 
 import UIKit
 
-var str = "Hello, playground"
+
+for k in 0 ... 100 {
+    if (k == 0) {
+        print(k);            // Caso no contemplado
+    }
+    if (k % 2 == 0){          // Regla 2
+        print("\(k) par");
+    } else {                  // Regla 3
+        if ((k % 5 == 0)){    // Regla 1
+            print("Bingo!");
+        }
+        print("\(k) impar");
+    }
+    if (k >= 30 && k <= 40) { // Regla 4
+        print("\(k) Viva Swift!");
+    }
+}
+
